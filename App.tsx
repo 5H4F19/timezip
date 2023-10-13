a1866206698aaa98eee147b7911c87a80a0b9929
shafik-soweb <shafiksoweb191@gmail.com>
2023-10-13 18:27:11 +0600

    renaming to timezip

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { useEffect } from 'react';
 import {
-  StatusBar, View
+  StatusBar, View, Button
 } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import SplashScreen from 'react-native-splash-screen';
@@ -14,27 +14,31 @@ const Stack = createNativeStackNavigator()
 
 const App = () => {
 
-  useEffect(() => {
-    SplashScreen.hide();
-  })
-
+  // useEffect(() => {
+  //   SplashScreen.hide();
+  // })
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <PortalProvider>
-        <View className='flex-1 bg-[#1A222D]'>
-          <StatusBar translucent backgroundColor="transparent" barStyle={'light-content'} />
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName={routes.ONBOARDING}>
-              {Routes.map(item => {
-                return (<Stack.Screen key={item.path} name={item.path} component={item.component} options={{ headerShown: false }} />)
-              })}
-            </Stack.Navigator>
-          </NavigationContainer>
-        </View>
-      </PortalProvider>
-
-    </GestureHandlerRootView>
+    <View>
+      <Button title='Pick a file or folder' />
+    </View>
   )
+  // return (
+  //   <GestureHandlerRootView style={{ flex: 1 }}>
+  //     <PortalProvider>
+  //       <View className='flex-1 bg-[#1A222D]'>
+  //         <StatusBar translucent backgroundColor="transparent" barStyle={'light-content'} />
+  //         <NavigationContainer>
+  //           <Stack.Navigator initialRouteName={routes.ONBOARDING}>
+  //             {Routes.map(item => {
+  //               return (<Stack.Screen key={item.path} name={item.path} component={item.component} options={{ headerShown: false }} />)
+  //             })}
+  //           </Stack.Navigator>
+  //         </NavigationContainer>
+  //       </View>
+  //     </PortalProvider>
+
+  //   </GestureHandlerRootView>
+  // )
 }
 
 export default App;
